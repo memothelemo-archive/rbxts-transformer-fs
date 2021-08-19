89f33bbcbcb8bf3f9098b11319d0a9fd6468d321
memothelemo <31132624+memothelemo@users.noreply.github.com>
2021-08-19 19:28:53 +0800

    Final release

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,19 @@ import Transformer from "./Transformer";
 import { TransformerConfig } from "./Transformer/config";
 import {} from "ts-expose-internals";
 import { transformSourceFile } from "./Transformer/functions/transformSourceFile";
+import { warn } from "./Shared/functions/warn";
 
 export default function (program: ts.Program, config: TransformerConfig) {
 	return (
 		context: ts.TransformationContext,
 	): ((file: ts.SourceFile) => ts.SourceFile) => {
 		const state = new Transformer.State(program, context, config);
+
+		// warn users that rbxts-transformer-path is outdated
+		warn(
+			"rbxts-transformer-path is deprecated, please use it at your own risk!",
+		);
+
 		state.printInVerbose("Initializing transformer state");
 
 		return file => transformSourceFile(state, file);
